9c6089e5ca68ef8220f29a8a60bbd8400a01ebe7
wang.yuan <wang.yuan@joydata.com>
2020-08-11 12:26:54 +0800

    update

--- app/Jobs/EntryDownload.php
+++ app/Jobs/EntryDownload.php
@@ -51,8 +51,8 @@ class EntryDownload implements ShouldQueue
         }
         $entity->channel_id = $channelId;
         $entity->title = $item->title;
-        $entity->published = Carbon::parse($item->published);
-        $entity->updated = Carbon::parse($item->published);
+        $entity->published = Carbon::parse($item->published)->setTimezone('Asia/Shanghai');
+        $entity->updated = Carbon::parse($item->published)->setTimezone('Asia/Shanghai');
         $this->setThumbnail($entity, $item);
         $entity->thumbnail_source = $this->getThumbnail($item);
         $entity->description = $item->media_group->media_description;
